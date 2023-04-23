e2c82fdb8f347f963ae2853d2422ecd478bd4666
ibrahim elsokary <107218787+ibrahimElsokkary@users.noreply.github.com>
2023-04-23 10:22:18 +0200

    create home repo

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,3 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:goldinia_app/core/models/gold_prices_model.dart';
+import 'package:goldinia_app/core/utils/errors/failure.dart';
+
 abstract class HomeRepo{
-   
+   Future<Either<Failure,GoldModel>> fetchGoldPriceToday(); 
 }
